7f1f74664a303cf99151583badf476bc7b8406b8
Rakshit Sathvara <rakshit.sathwara@tmspl.com>
2018-09-20 23:27:45 +0530

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+
+    public TextField et_private_key, et_public_key, et_secret_key;
+    public TextArea et_request, et_response;
+
+    @FXML
+    public void onEncryptBtnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void onDecryptBtnClick(ActionEvent event) {
+
+    }
+}
